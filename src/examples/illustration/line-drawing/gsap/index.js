0860cdabd03ff4d06e83e4b257c27ec9b12b5d20
Firestorm980 <Firestorm980@users.noreply.github.com>
2023-03-15 16:00:03 +0000

    Automated build cd746a3

--- src/examples/illustration/line-drawing/gsap/index.js
+++
@@ -1,9 +0,0 @@
-import gsap from 'gsap'
-
-const tl = gsap.timeline({ yoyo: true, repeat: Infinity })
-
-tl.fromTo(
-  '.logo, .ports, .outline',
-  { strokeDashoffset: 1 },
-  { strokeDashoffset: 0, duration: 5, autoRound: false }
-)
