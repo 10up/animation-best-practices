cc8ce074a40c5cefb5974ccb56c0909e0fa82122
Firestorm980 <Firestorm980@users.noreply.github.com>
2023-03-16 15:29:00 +0000

    Automated build 102f38f

--- src/examples/ui/page-transition/css/index.js
+++
@@ -1,9 +0,0 @@
-import Swup from 'swup'
-import SwupA11yPlugin from '@swup/a11y-plugin'
-import SwupBodyClassPlugin from '@swup/body-class-plugin'
-
-// eslint-disable-next-line no-unused-vars
-const swup = new Swup({
-  containers: ['#container'],
-  plugins: [new SwupA11yPlugin(), new SwupBodyClassPlugin()]
-})
