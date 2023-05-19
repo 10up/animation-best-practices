154be7a8b669546edc4d8a572c007a04a59b474b
Dylan James Wagner <dylan.james.wagner@get10up.com>
2023-05-19 13:07:44 -0400

    Encapsulate sticky header behavior script

--- src/examples/ui/sticky-header/css/index.js
+++ src/examples/ui/sticky-header/css/index.js
@@ -79,88 +79,94 @@ function setScrollPositionThreshold(headerHeight) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-	const header = document.querySelector('.site-header');
 
-	/** Abort if header does not have the sticky modifier class. */
-	if (!header.classList.contains('site-header--sticky')) {
-		return;
-	}
+	/**
+	 * Sticky header behavior
+	 */
+	(function () {
+		const header = document.querySelector('.site-header');
 
-	let headerHeight = getOffsetHeight(header);
-	let scrollPosition = 0;
-	let scrollPositionThreshold = setScrollPositionThreshold(headerHeight);
-	let scrollDistance;
-	let scrollDistanceAbs;
-	let scrollDistanceThreshold = 24;
-	let scrollHideTimeout = 2000;
-	let scrollDirection;
-	let scrollDirectionCurrent;
-
-	/** Update style property. */
-	setRootStyleProperty('--header-height', headerHeight);
-
-	/** Hide sticky header by adding a modifier class. */
-	function hideStickyHeader() {
-		header.classList.add('site-header--hide');
-	}
-
-	/** Show sticky header by removing a modifier class. */
-	function showStickyHeader() {
-		header.classList.remove('site-header--hide');
-	}
-
-	/** Resize, recalculate header height and scroll position threshold. */
-	window.addEventListener('resize', throttle(() => {
-		headerHeight = getOffsetHeight(header);
-		scrollPositionThreshold = setScrollPositionThreshold(headerHeight);
-	}, 100));
-
-	/** Wait to initialize hiding behavior. (For local links on initial load.) */
-	setTimeout(() => {
-
-		/** Have we scrolled past the scroll position threshold? Hide the sticky header. */
-		if (window.pageYOffset > scrollPositionThreshold) {
-			hideStickyHeader();
+		/** Abort if header does not have the sticky modifier class. */
+		if (!header.classList.contains('site-header--sticky')) {
+			return;
 		}
 
-		/** Scroll, calculate sticky header hide and show behavior. */
-		document.addEventListener('scroll', throttle(() => {
-			scrollDistance = window.pageYOffset - scrollPosition;
+		let headerHeight = getOffsetHeight(header);
+		let scrollPosition = 0;
+		let scrollPositionThreshold = setScrollPositionThreshold(headerHeight);
+		let scrollDistance;
+		let scrollDistanceAbs;
+		let scrollDistanceThreshold = 24;
+		let scrollHideTimeout = 2000;
+		let scrollDirection;
+		let scrollDirectionCurrent;
+
+		/** Update style property. */
+		setRootStyleProperty('--header-height', headerHeight);
+
+		/** Hide sticky header by adding a modifier class. */
+		function hideStickyHeader() {
+			header.classList.add('site-header--hide');
+		}
+
+		/** Show sticky header by removing a modifier class. */
+		function showStickyHeader() {
+			header.classList.remove('site-header--hide');
+		}
+
+		/** Resize, recalculate header height and scroll position threshold. */
+		window.addEventListener('resize', throttle(() => {
+			headerHeight = getOffsetHeight(header);
+			scrollPositionThreshold = setScrollPositionThreshold(headerHeight);
+		}, 100));
+
+		/** Wait to initialize hiding behavior. (For local links on initial load.) */
+		setTimeout(() => {
 
 			/** Have we scrolled past the scroll position threshold? Hide the sticky header. */
 			if (window.pageYOffset > scrollPositionThreshold) {
-				scrollDistanceAbs = Math.abs(scrollDistance);
-
-				/** Have we scrolled past the scroll distance threshold? Hide or show the header. */
-				if (scrollDistanceAbs > scrollDistanceThreshold) {
-					scrollDirection = Math.sign(scrollDistance);
-
-					/** Have we changed scroll direction? */
-					if (scrollDirectionCurrent !== scrollDirection) {
-
-						/** Update scroll direction. */
-						scrollDirectionCurrent = Math.sign(scrollDirection);
-
-						/** Are we scrolling up? Show the sticky header. */
-						if (scrollDirection === -1) {
-							showStickyHeader();
-						}
-
-						/** Are we scrolling down? Hide the sticky header. */
-						if (scrollDirection === 1) {
-							hideStickyHeader();
-						}
-					}
-				}
-			} else{
-				/** Are we at the top of the page? Show the sticky header. */
-				showStickyHeader();
+				hideStickyHeader();
 			}
 
-			/** Update scroll position. */
-			scrollPosition = window.pageYOffset;
+			/** Scroll, calculate sticky header hide and show behavior. */
+			document.addEventListener('scroll', throttle(() => {
+				scrollDistance = window.pageYOffset - scrollPosition;
 
-		}, 200));
+				/** Have we scrolled past the scroll position threshold? Hide the sticky header. */
+				if (window.pageYOffset > scrollPositionThreshold) {
+					scrollDistanceAbs = Math.abs(scrollDistance);
 
-	}, scrollHideTimeout);
+					/** Have we scrolled past the scroll distance threshold? Hide or show the header. */
+					if (scrollDistanceAbs > scrollDistanceThreshold) {
+						scrollDirection = Math.sign(scrollDistance);
+
+						/** Have we changed scroll direction? */
+						if (scrollDirectionCurrent !== scrollDirection) {
+
+							/** Update scroll direction. */
+							scrollDirectionCurrent = Math.sign(scrollDirection);
+
+							/** Are we scrolling up? Show the sticky header. */
+							if (scrollDirection === -1) {
+								showStickyHeader();
+							}
+
+							/** Are we scrolling down? Hide the sticky header. */
+							if (scrollDirection === 1) {
+								hideStickyHeader();
+							}
+						}
+					}
+				} else{
+					/** Are we at the top of the page? Show the sticky header. */
+					showStickyHeader();
+				}
+
+				/** Update scroll position. */
+				scrollPosition = window.pageYOffset;
+
+			}, 200));
+
+		}, scrollHideTimeout);
+	})();
 });
